20aef381136450a54bb0fb1485c64d6418fb242d
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-07-26 23:29:06 +0900

    statusがunapprovedであるCommentがあるEntryを抽出するメソッドを作成

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,14 @@
 class Entry < ApplicationRecord
   has_many :comments
   belongs_to :blog
+
+  def self.entries_having_unapproved_comment
+    comments = Comment.where(status: "unapproved")
+    entries = []
+    comments.each do |comment|
+      entries << comment.entry
+    end
+    entries
+  end
+
 end
