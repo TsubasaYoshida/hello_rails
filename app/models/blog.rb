cd65e300ef44fad92d263ac329ca27401632f2a3
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-07-27 11:35:11 +0900

    まだEntryを書いていないBlogを抽出するメソッドを作成

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,13 @@
 class Blog < ApplicationRecord
   has_many :entries
+
+  def self.blog_have_no_entry
+    all_blog = Blog.all
+    ret = []
+    all_blog.each do |blog|
+      ret << blog if blog.entries.count == 0
+    end
+    ret
+  end
+
 end
