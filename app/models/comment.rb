441556d65ed7ef7507bb6d7ba2f2dc25e1422fc2
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-07-27 11:53:38 +0900

    Blogのid:1に紐づくすべてのCommentを抽出するメソッドをリファクタリング

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :entry
 
   def self.comments_related_blog_1
-    entries = Entry.where(blog_id: 1)
     comments = []
-    entries.each do |entry|
-      comments_narrowed_by_entry = Comment.where(entry_id: entry.id)
-      comments_narrowed_by_entry.each do |comment|
+    Blog.find_by(id: 1).entries.each do |entry|
+      entry.comments.each do |comment|
         comments << comment
       end
     end
