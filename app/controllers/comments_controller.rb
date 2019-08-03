5864f2be70d706400488ed039fdf9a5e6521cbeb
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-08-04 01:16:20 +0900

    全ての要件を満たすようにブラッシュアップを行う

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,39 @@
 class CommentsController < ApplicationController
-  def approve
-    @comment = Comment.find(params[:id])
-    @comment.update(status: "approved")
-    @entry = @comment.entry
-    redirect_to blog_entry_url(@entry.blog, @entry)
+  before_action :set_comment, only: [:approve, :destroy]
+  before_action :set_blog_entry, only: [:create, :approve, :destroy]
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.assign_attributes(entry_id: @entry.id, status: "unapproved")
+    if @entry.blog_id == @blog.id
+      @comment.save
+    end
+    redirect_to blog_entry_url(@blog, @entry)
   end
+
+  def destroy
+    @comment.destroy
+    redirect_to blog_entry_url(@blog, @entry)
+  end
+
+  def approve
+    @comment.update(status: "approved")
+    redirect_to blog_entry_url(@blog, @entry)
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def set_blog_entry
+    @blog = Blog.find(params[:blog_id])
+    @entry = Entry.find(params[:entry_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:status, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :blogs do
-    resources :entries
-  end
-  resources :comments do
-    patch :approve, on: :member
+    resources :entries, except: [:index] do
+      resources :comments, only: [:create, :destroy] do
+        patch :approve, on: :member
+      end
+    end
   end
 end
