e89466bb14b6e83a8fada27ee66d2ef92bad9a2a
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-08-09 23:17:16 +0900

    entry_spec.rbのバリデーションテストを記述

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -6,13 +6,25 @@ RSpec.describe Entry, type: :model do
     blog = Blog.create(
         id: 1,
         title: "タイトル1",
-        )
+    )
     entry = Entry.new(
         title: "タイトル1",
         body: "本文1",
         blog_id: 1,
     )
     expect(entry).to be_valid
+  end
+
+  it "タイトルがなければ無効な状態であること" do
+    entry = Entry.new(title: nil)
+    entry.valid?
+    expect(entry.errors[:title]).to include("can't be blank")
+  end
+
+  it "本文がなければ無効な状態であること" do
+    entry = Entry.new(body: nil)
+    entry.valid?
+    expect(entry.errors[:body]).to include("can't be blank")
   end
 
 end
