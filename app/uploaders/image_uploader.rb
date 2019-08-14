4de4855c37446b43dd5d714c02f491e88f56d134
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-08-15 08:10:14 +0900

    サムネイル表示を実装する

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,16 +1,39 @@
 class ImageUploader < CarrierWave::Uploader::Base
+
+  include CarrierWave::RMagick
+
   storage :file
-  # storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  # ファイルサイズに制限をつける
+  def size_range
+    1..5.megabytes
+  end
+
+  # 画像の上限を640x480にする
+  process :resize_to_limit => [640, 640]
+
+  # サムネイル保存する
+  version :thumb do
+    process :resize_to_limit => [320, 320]
+  end
+
+  # 保存形式
+  process :convert => 'jpg'
+
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
 
-  # リネームする
+  # 拡張子が同じでないとGIFをJPGとかにコンバートできない
+  def filename
+    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+  end
+
+  # ファイル名を日付にすると不具合が出る
   def filename
     "#{secure_token}.#{file.extension}" if original_filename.present?
   end
