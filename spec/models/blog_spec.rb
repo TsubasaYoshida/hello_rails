a60f18ed35729eb353f69979551c19ddbe7819a8
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-08-22 08:35:21 +0900

    factory_botでテストコードを書き直してみる

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,14 +1,29 @@
 require 'rails_helper'
 
 describe Blog do
+  # it "titleがあれば有効な状態であること" do
+  #   blog = Blog.new
+  #   blog.title = "ブログタイトル"
+  #   expect(blog.valid?).to be_truthy
+  # end
+
+  # it "titleがなければ無効な状態であること" do
+  #   blog = Blog.new
+  #   expect(blog.valid?).to be_falsey
+  # end
+
+  # 上のテストをfactory_botで書き直してみる
+  before do
+    @blog = build(:blog)
+  end
+
   it "titleがあれば有効な状態であること" do
-    blog = Blog.new
-    blog.title = "ブログタイトル"
-    expect(blog.valid?).to be_truthy
+    expect(@blog.valid?).to be_truthy
   end
 
   it "titleがなければ無効な状態であること" do
-    blog = Blog.new
-    expect(blog.valid?).to be_falsey
+    @blog.title = ''
+    expect(@blog.valid?).to be_falsey
   end
+
 end
